febcc0d63e2db0679c911eb2ae817892e100590c
Nikolaus Rosenmayr <nr@firecoder.com>
2022-11-24 14:03:20 +0100

    chore(build): basic project setup with Vite
    Signed-off-by: Nikolaus Rosenmayr <nr@firecoder.com>

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import type { UserConfigExport } from "vite";
+
+import path from "path";
+import { defineConfig } from "vite";
+
+interface PackageJson {
+    name: string,
+    main: string,
+    module: string,
+    types: string,
+    version: string,
+    dependencies: Record<string, string>,
+    peerDependencies?: Record<string, string>,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require("./package.json") as PackageJson;
+
+
+// https://vitejs.dev/config/
+export const BaseConfig: UserConfigExport = {
+    plugins: [],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            formats: ["es"],
+        },
+        rollupOptions: {
+            external: [
+                ...Object.getOwnPropertyNames(pkg.dependencies || {}),
+                ...Object.getOwnPropertyNames(pkg.peerDependencies || {}),
+                "tslib",
+            ],
+        },
+    },
+};
+
+export default defineConfig(BaseConfig);
